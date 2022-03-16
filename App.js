8354bf3aaa8f94ac5bbad48cd9280a2da89657b5
yurimarim <yurimarim@hotmail.com>
2022-03-16 16:13:14 -0300

    fix: refactored code

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-// import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar'
-import firebase from './src/services/firebaseConnection'
 import { Routes } from './src/routes'
 
 export default function App() {
