e00b2b2ca288962090e10302eeb3777b411d5d26
yurimarim <yurimarim@hotmail.com>
2022-04-19 18:03:28 -0300

    historicList component

---
+++ src/components/HistoricList/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container, Type, IconView, TypeText, ValueText } from './styles'
+import { Feather } from '@expo/vector-icons'
+
+export function HistoricList({ data }) {
+  return (
+    <Container>
+      <Type>
+        <IconView type={data.type}>
+          <Feather
+            name={data.type === 'Despesa' ? 'arrow-down' : 'arrow-up'}
+            size={20}
+            color="#fff"
+          />
+          <TypeText>{data.type}</TypeText>
+        </IconView>
+      </Type>
+      <ValueText>{`R$${data.value}`}</ValueText>
+    </Container>
+  )
+}
---
+++ src/components/HistoricList/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  margin-bottom: 5px;
+  padding: 10px;
+  box-shadow: 2px 2px rgba(0, 0, 0, 0.4);
+  background-color: rgba(0, 0, 0, 0.02);
+`
+export const Type = styled.View`
+  flex-direction: row;
+`
+export const IconView = styled.View`
+  flex-direction: row;
+  background-color: ${props =>
+    props.type === 'Despesa' ? '#c62c36' : '#049301'};
+  padding: 3px 8px;
+  border-radius: 7px;
+`
+export const TypeText = styled.Text`
+  font-size: 16px;
+  font-weight: bold
+  color: #fff;
+`
+export const ValueText = styled.Text`
+  font-size: 22px;
+  font-weight: bold;
+  color: #222;
+`
