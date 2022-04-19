686d0c4e3941fb8c70e2b61741ccee1458dda7b5
yurimarim <yurimarim@hotmail.com>
2022-04-18 21:21:53 -0300

    setItem async storage and getItem

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,10 +1,54 @@
-import React, { useState, createContext } from 'react'
+import React, { useState, useEffect, createContext } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import firebase from '../services/firebaseConnection'
 
 export const AuthContext = createContext({})
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function loadStorage() {
+      const storageUser = await AsyncStorage.getItem('@auth_user')
+
+      if (storageUser) {
+        setUser(JSON.parse(storageUser))
+        setLoading(false)
+      }
+
+      setLoading(false)
+    }
+
+    loadStorage()
+  }, [])
+
+  // Sign In user
+  async function signIn(email, password) {
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(async value => {
+        let uid = value.user.uid
+        await firebase
+          .database()
+          .ref('users')
+          .child(uid)
+          .once('value')
+          .then(snapshot => {
+            let data = {
+              uid: uid,
+              name: snapshot.val().name,
+              email: value.user.email
+            }
+            setUser(data)
+            storageUser(data)
+          })
+      })
+      .catch(err => {
+        alert(`Error: ${err.code}`)
+      })
+  }
 
   // Sign Up user
   async function signUp(email, password, name) {
@@ -28,12 +72,19 @@ export function AuthProvider({ children }) {
               email: value.user.email
             }
             setUser(data)
+            storageUser(data)
           })
       })
   }
 
+  async function storageUser(data) {
+    await AsyncStorage.setItem('@auth_user', JSON.stringify(data))
+  }
+
   return (
-    <AuthContext.Provider value={{ signed: !!user, user, signUp }}>
+    <AuthContext.Provider
+      value={{ signed: !!user, user, signUp, signIn, loading }}
+    >
       {children}
     </AuthContext.Provider>
   )
